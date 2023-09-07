6ea26288d00c45d12caf45b73695aa36673b3ff1
NekruzRakhimov <nekruzusa111@gmail.com>
2023-09-07 15:44:13 +0500

    added animation

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -24,6 +24,8 @@ const Ball = ({number = 10}) => {
         <div className="ball"
              style={{
                  background: colors[colorIndex - 1],
+                 boxShadow: `0px 0px 16px ${colors[colorIndex - 1]}`,
+                 border: "2px solid white",
                  animationDelay: `${(0.5 * Math.random()) + 0.5}s`,
                  animation: `ball-scale ${(0.5 * Math.random()) + 0.5}s cubic-bezier(0,0, 0.5, 1.25)`
              }}>
--- src/pages/second_page.jsx
+++ src/pages/second_page.jsx
@@ -1,5 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import Ball from "../components/Ball";
+import myWebm from "../files/balls.webm"
+import {Howl, Howler} from 'howler';
+import sound1 from "../files/rolling1.wav"
+import reveal1 from "../files/reveal1.mp3"
+import reveal2 from "../files/reveal2.mp3"
+
+
+let sound = new Howl({
+    src: [sound1],
+    rate: 0.75
+});
+
+let reveal = new Howl({
+    src: [reveal1, reveal2]
+});
+
 
 const getDigitsArray = (number) => {
     let digits = [];
@@ -15,54 +31,83 @@ const getDigitsArray = (number) => {
 };
 
 const SecondPage = () => {
+    const videoPlayer = useRef()
+
     const [randomNumber, setRandomNumber] = useState(0);
+    const [videoLoaded, setVideoLoaded] = useState(false);
+
 
     const handleMessage = (_e) => {
         setRandomNumber(null);
-        console.log(1)
-
+        sound.play()
+        if (videoPlayer.current) {
+            videoPlayer.current.currentTime = 0
+            videoPlayer.current.play()
+        }
         setTimeout(() => {
-            // console.log(_e)
-            console.log(2)
-            const message = JSON.parse(localStorage.getItem('randomNumber')); // Получаем сообщение из localStorage
+            const message = JSON.parse(localStorage.getItem('randomNumber'));
             if (message) {
-
                 setRandomNumber(message.data);
-                // localStorage.removeItem('randomNumber'); // Удаляем сообщение из localStorage
+                reveal.play()
             }
 
-        }, 500)
-
-
+        }, 5000);
     };
 
     useEffect(() => {
+        handleMessage();
+        window.addEventListener('storage', handleMessage);
 
-        handleMessage()
-        // const handleMessage = (event) => {
-        //     const message = JSON.parse(localStorage.getItem('randomNumber')); // Получаем сообщение из localStorage
-        //     if (message) {
-        //         setRandomNumber(message.data);
-        //         localStorage.removeItem('randomNumber'); // Удаляем сообщение из localStorage
-        //     }
-        // };
-
-        window.addEventListener('storage', handleMessage); // Слушаем события хранилища
-
-        // return () => {
-        //     window.removeEventListener('storage', handleMessage);
-        // };
+        return () => {
+            window.removeEventListener('storage', handleMessage);
+        };
     }, []);
 
-    let randomItemNumbers = getDigitsArray(randomNumber)
+    const handleVideoLoad = () => {
+        setVideoLoaded(true);
+    };
+
+    let randomItemNumbers = getDigitsArray(randomNumber);
+
+    // useEffect(() => {
+    //     const videoElement = document.getElementById('video');
+    //     videoElement.addEventListener('loadeddata', handleVideoLoad);
+    //
+    //     return () => {
+    //         videoElement.removeEventListener('loadeddata', handleVideoLoad);
+    //     };
+    // }, []);
+
 
     return (
-        <div>
-            <div className="id-picker-content-main-items">
-                {randomItemNumbers.map((v, i) =>
-                    <Ball number={v}/>
-                )}
-            </div>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            {/*<video width="320" height="240" controls>*/}
+            {/*    <source src={myVideo} type="video/mp4"/>*/}
+            {/*    Your browser does not support the video tag*/}
+            {/*</video>*/}
+            <video id="video" width="960" ref={videoPlayer} onLoadedData={handleVideoLoad} style={{
+                position: "absolute",
+                left: "calc(50% - 480px)",
+
+            }}>
+                {/*<source src="https://rotato.netlify.app/alpha-demo/movie-hevc.mov" type='video/mp4'; codecs="hvc1">*/}
+                {/*<source src={myMov} type='video/mp4; codecs="hvc1"'/>*/}
+                <source src={myWebm} type='video/webm'/>
+                {/*<source src="../../public/temp.mp4" type='video/mp4' />*/}
+            </video>
+            {!videoLoaded && <div>Loading video...</div>}
+            {videoLoaded && (
+                <div className="id-picker-content-main-items">
+                    {randomItemNumbers.map((v, i) => (
+                        <Ball number={v} key={i}/>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
